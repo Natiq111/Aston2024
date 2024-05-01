ba61c18a61f81a70707da7349a243d3a23d29c54
Natiq111 <163619656+Natiq111@users.noreply.github.com>
2024-05-01 21:16:18 +0700

    Add files via upload

---
+++ Lesson6.java
@@ -0,0 +1,7 @@
+public class Lesson6 {
+    public static void main(String[] args) {
+     
+        Worker wrk1 = new Worker("Пашнева Наталья Владимировна", "тестировщик", "a_sfera@mail.ru", "89132292809", 50000, 40);
+        wrk1.Print();
+    }
+}
